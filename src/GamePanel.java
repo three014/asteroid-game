6ec8b71e059cbc9cf41c0da19963fa1ccf95a66b
three014 <106114579+three014@users.noreply.github.com>
2022-05-23 23:16:51 -0500

    started work on laser class
    Right now a singular laser shows up on screen I click, but it only moves to the right, and only when the left side of the screen is clicked

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -11,14 +11,17 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int UPDATES_PER_SEC = 60;
     static final float DIAG_CORRECTION = 0.414214f; // ~= sqrt(2) - 1
     static final double XY_CORRECTION = Math.sqrt((DIAG_CORRECTION * DIAG_CORRECTION) / 2);
+    // final int MAX_LASERS = 3;
     static final Point offset = new Point();
     static int xFire = -1;
     static int yFire = -1;
     static boolean shoot = false;
     int shipX = (SCREEN_WIDTH - UNIT_SIZE) / 2;
     int shipY = (SCREEN_HEIGHT - UNIT_SIZE) / 2;
-    int health = 3;
+    // int health = 3;
     boolean running = false;
+    // laser[] l = new laser[MAX_LASERS]; FIXME: turn into array of lasers when laser class works properly
+    laser l;
     Timer timer;
     Random rand;
 
@@ -34,7 +37,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void startGame() {
         running = true;
-        timer = new Timer((int)(1000/UPDATES_PER_SEC), this);
+        timer = new Timer((1000/UPDATES_PER_SEC), this);
         timer.start();
     }
 
@@ -50,10 +53,19 @@ public class GamePanel extends JPanel implements ActionListener {
             g.setColor(Color.gray);
             g.fillRect(shipX, shipY, UNIT_SIZE, UNIT_SIZE);
 
+
+            // draw laser
+            if (l != null) { // FIXME: turn l into array of lasers when laser class works
+                g.setColor(Color.green);
+                g.fillOval(l.laserX, l.laserY, l.LASER_SIZE, l.LASER_SIZE);
+            }
+
         }
     }
 
     public void move() {
+
+        // moving the spaceship part 1
         switch (offset.x) {
             case -1: shipX = shipX - (int)(0.25 * UNIT_SIZE); break;
             case 1: shipX = shipX + (int)(0.25 * UNIT_SIZE); break;
@@ -63,7 +75,9 @@ public class GamePanel extends JPanel implements ActionListener {
             case -1: shipY = shipY + (int)(0.25 * UNIT_SIZE); break;
         }
 
-        // checking for diagonal movement to correct for its larger move size
+        /* moving the spaceship part 2:
+         * checking for diagonal movement to correct for its larger move size
+         */
         if (offset.x == 1 && offset.y == 1) {
             shipY = shipY + (int)(XY_CORRECTION * 0.25 * UNIT_SIZE);
             shipX = shipX - (int)(XY_CORRECTION * 0.25 * UNIT_SIZE);
@@ -80,6 +94,7 @@ public class GamePanel extends JPanel implements ActionListener {
             shipY = shipY - (int)(XY_CORRECTION * 0.25 * UNIT_SIZE);
             shipX = shipX - (int)(XY_CORRECTION * 0.25 * UNIT_SIZE);
         }
+
     }
 
     public void aim() {
@@ -88,6 +103,10 @@ public class GamePanel extends JPanel implements ActionListener {
         }
         else {
             setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+        }
+
+        if (shoot) {
+            l = new laser();
         }
     }
 
@@ -105,6 +124,10 @@ public class GamePanel extends JPanel implements ActionListener {
             move();
             aim();
             checkCollisions();
+
+            if (l != null) { // FIXME: turn l into array of lasers when laser class is fixed
+                l.moveLaser();
+            }
         }
         repaint();
     }
@@ -113,26 +136,31 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
-    public class laser {
+    public class laser { // FIXME: laser moves when left side of screen is clicked, and only to the right
         final int LASER_SIZE = (int)(UNIT_SIZE * 0.25);
 
-        int slope;
-        int laserX;
-        int laserY;
+        int laserY, laserX; // vector A
+        int absV; // unit vector of A
 
         laser() {
-            // starting laser coordinates
-            if (xFire >= 0 && yFire >= 0) {
+
+            // see if it's okay to shoot a laser in the first place
+            if (shoot) {
+
+                // starting laser coordinates
                 laserX = shipX + (UNIT_SIZE / 2);
                 laserY = shipY + (UNIT_SIZE / 2);
+
+                // find laser slope
+                absV = (int)Math.sqrt((xFire * xFire) + (yFire * yFire));
+
+                shoot = false;
             }
+        }
 
-            // find laser slope
-            slope = (yFire - laserY) / (xFire - laserX);
-
-
-
-            shoot = false;
+        void moveLaser() {
+            laserX = laserX + ((laserX / absV) * LASER_SIZE);
+            laserY = laserY + ((laserY / absV) * LASER_SIZE);
         }
     }
 }
