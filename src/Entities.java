08081e84a7af416a5e095c84e726aa247de73a56
three014 <106114579+three014@users.noreply.github.com>
2022-06-08 12:57:40 -0500

    Set up asteroid collisions with lasers and spaceship
    shooting asteroids will slowly increase the rate at which asteroids spawn, and hitting an asteroid with your spaceship will lower your health by one

---
+++ src/Entities.java
@@ -0,0 +1,139 @@
+import java.awt.*;
+
+public class Entities {
+
+    public static class asteroid extends Rectangle{
+
+        // public int locX, locY;
+        public double moveX, moveY;
+
+        asteroid() {
+
+            final int SIZE = determineSize();
+            final double SPEED_CONSTANT = (int) (GamePanel.SHIP_SPEED * 0.9);
+            final double ASTEROID_SPEED = ( ( (double) GamePanel.SHIP_SIZE / (double) SIZE ) * SPEED_CONSTANT );
+            width = SIZE;
+            height = SIZE;
+
+            int dirX, dirY;
+            int randomFactorX = GamePanel.rand.nextInt(4);
+            int randomFactorY;
+            double absV;
+
+            // determine starting location and direction
+            switch (randomFactorX) {
+                case 0 -> {
+                    x = GamePanel.rand.nextInt(SIZE) - SIZE - SIZE;
+                    dirX = 200 + GamePanel.rand.nextInt(300);
+                    randomFactorY = 2;
+                }
+                case 1 -> {
+                    x = GamePanel.rand.nextInt(SIZE) + GamePanel.SCREEN_WIDTH;
+                    dirX = -500 + GamePanel.rand.nextInt(300);
+                    randomFactorY = 2;
+                }
+                case 2, 3 -> {
+                    x = GamePanel.rand.nextInt(GamePanel.SCREEN_WIDTH - GamePanel.DIRECTION_LIMIT * 2) + GamePanel.DIRECTION_LIMIT;
+                    dirX = -400 + GamePanel.rand.nextInt(800);
+                    randomFactorY = GamePanel.rand.nextInt(2);
+                }
+                default -> {
+                    x = -SIZE;
+                    dirX = 500;
+                    randomFactorY = 0;
+                }
+            }
+            switch (randomFactorY) {
+                case 0 -> {
+                    y = GamePanel.rand.nextInt(SIZE) - SIZE - SIZE;
+                    dirY = 200 + GamePanel.rand.nextInt(300);
+                }
+                case 1 -> {
+                    y = GamePanel.rand.nextInt(SIZE) + GamePanel.SCREEN_HEIGHT;
+                    dirY = -500 + GamePanel.rand.nextInt(300);
+                }
+                case 2 -> {
+                    y = GamePanel.rand.nextInt(GamePanel.SCREEN_HEIGHT - GamePanel.DIRECTION_LIMIT * 2) + GamePanel.DIRECTION_LIMIT;
+                    dirY = -400 + GamePanel.rand.nextInt(800);
+                }
+                default -> {
+                    y = -SIZE;
+                    dirY = 500;
+                }
+            }
+            // More debugging
+            System.out.println("(" + dirX + ", " + dirY + ")");
+
+            // find normalizing factor for unit vector of initial asteroid trajectory
+            absV = Math.sqrt( Math.pow( dirX, 2.0 ) + Math.pow( dirY, 2.0 ) );
+
+            moveX = ( ( (double) dirX ) / absV ) * ASTEROID_SPEED;
+            moveY = ( ( (double) dirY ) / absV ) * ASTEROID_SPEED;
+        }
+
+        void move() {
+            x += (int) moveX;
+            y += (int) moveY;
+        }
+
+        int determineSize() {
+
+            int chance = GamePanel.rand.nextInt(100);
+
+            if (chance < 60) {
+                return GamePanel.SHIP_SIZE;
+            }
+            else if (chance < 80) {
+                return (int) (GamePanel.SHIP_SIZE * 1.25);
+            }
+            else if (chance < 90) {
+                return (int) (GamePanel.SHIP_SIZE * 0.4);
+            }
+            else {
+                return GamePanel.SHIP_SIZE * 2;
+            }
+        }
+    }
+
+    public static class laser extends Rectangle{
+
+        //public int laserY, laserX;
+        private final int moveX, moveY;
+
+        laser(int X, int Y) {
+
+            final int LASER_SPEED = GamePanel.SHIP_SPEED * 2;
+            final int LASER_SIZE = (int) (GamePanel.SHIP_SIZE * 0.25);
+            width = LASER_SIZE;
+            height = LASER_SIZE;
+
+            int aimX, aimY;
+            double absV;
+
+            // starting laser coordinates
+            x = X + (int) (GamePanel.SHIP_SIZE * 0.5);
+            y = Y + (int) (GamePanel.SHIP_SIZE * 0.5);
+            aimX = GamePanel.xFire;
+            aimY = GamePanel.yFire;
+
+            // find normalizing factor for unit vector of laser trajectory
+            absV = Math.sqrt( ( ( aimX - X ) * ( aimX - X ) ) + ( ( aimY - Y ) * ( aimY - Y ) ) );
+
+            // combine into actual laser beam movement
+            moveX = (int) ( ( (double) ( aimX - X ) / absV ) * LASER_SPEED );
+            moveY = (int) ( ( (double) ( aimY - Y ) / absV ) * LASER_SPEED );
+
+                // Debugging
+                //System.out.println("Normalizing factor: " + absV);
+                //System.out.println("(" + aimX + ", " + aimY + ")");
+                //System.out.println("(" + directionX + ", " + directionY + ")");
+
+            GamePanel.canShoot = false;
+        }
+
+        void moveLaser() {
+            x += moveX;
+            y += moveY;
+        }
+    }
+}
